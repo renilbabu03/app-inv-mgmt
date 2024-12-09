af9517c81ee7105b609da78b5ad783af26a01fd6
Renil <renilbabu03@gmail.com>
2024-12-09 18:09:34 +0530

    modified

--- src/services/Product/productService.ts
+++ src/services/Product/productService.ts
@@ -5,5 +5,5 @@ export const fetchProducts = () => axiosInstance.get('/products');
 export const fetchProductById = (id: string) => axiosInstance.get(`/products/${id}`);
 
 export const createProduct = (product: ProductFormData) => axiosInstance.post('/products', product)
-export const updateProduct = (id: number, product: ProductFormData) => axiosInstance.post(`/products/${id}`, product)
+export const updateProduct = (id: number, product: ProductFormData) => axiosInstance.put(`/products/${id}`, product)
 export const deleteProductById = (id: number) => axiosInstance.delete(`/products/${id}`) 
